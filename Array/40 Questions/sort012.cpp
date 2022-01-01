4fdca302bc72d9810debb6ccf6dfcd9263ce1875
Nandani-koli <70843941+Nandani-koli@users.noreply.github.com>
2022-01-01 12:21:17 +0530

    Update sort012.cpp

--- Array/40 Questions/sort012.cpp
+++ Array/40 Questions/sort012.cpp
@@ -1,10 +1,8 @@
-// { Driver Code Starts
+
 #include<bits/stdc++.h>
 using namespace std;
+//Method 1:-
 
-
-
- // } Driver Code Ends
 class Solution
 {
     public:
@@ -44,7 +42,35 @@ class Solution
     }
 };
 
-// { Driver Code Starts.
+//method 2:-
+// class Solution
+// {
+//     public:
+//     void sort012(int a[], int n)
+//     {
+//         // coode here 
+//         int low = 0,mid = 0, high = n-1;
+//     while(mid <= high)
+//     {
+//         if(a[mid] == 0)
+//         {
+//             swap(a[low],a[mid]);
+// 			low++;
+// 			mid++;
+//         }
+//         else if(a[mid] == 2)
+//         {
+//             swap(a[high],a[mid]);
+// 			high--;
+//         }
+//         else{
+//             mid++;
+//         }
+//     }
+//     }
+// };
+
+
 int main() {
 
     int t;
